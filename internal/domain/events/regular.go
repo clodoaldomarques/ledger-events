e7f85be1f0e988c5a55a517fbc3f7bf4b7993265
clodoaldomarques <clodoaldomarques@gmail.com>
2026-06-29 23:10:49 -0300

    quebrando o processamento para atender os diversos fluxos

---
+++ internal/domain/events/regular.go
@@ -0,0 +1,36 @@
+package events
+
+import (
+	"github.com/clodoaldomarques/ledger-events/internal/domain/configs"
+	"github.com/clodoaldomarques/ledger-events/internal/domain/expression"
+	"github.com/shopspring/decimal"
+)
+
+func ProcessRegular(c configs.Config, e *Event, amounts, fees map[string]decimal.Decimal) error {
+	e.Description = c.Description
+
+	for _, en := range c.RetrieveEntryByProducer(configs.Migration) {
+		calculated, err := expression.Calculate(en.Expression, amounts, fees)
+		if err != nil {
+			return err
+		}
+
+		entry := Entry{
+			EntryTypeID: en.ScriptID,
+			Amount:      calculated,
+			Description: en.Description,
+		}
+
+		if en.CreditAccount != nil {
+			entry.CreditAccount = en.CreditAccount.Number
+		}
+
+		if en.DebitAccount != nil {
+			entry.DebitAccount = en.DebitAccount.Number
+		}
+
+		e.Entries = append(e.Entries, entry)
+	}
+
+	return nil
+}
